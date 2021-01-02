b6efa0fc258b24fc7a342d6185660d6476fb552b
SirOphis <moses.belfatmi@gmail.com>
2021-01-03 00:39:20 +0100

    Index without popup

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="FlashCards for memorizing and learning new things!">
+    <meta name="author" content="Moussa Belfatmi">
+    <link rel="stylesheet" href="./css/main.css">
+    <link rel="ico" href="favico.ico">
+    <title>FlashCards - Welcome</title>
+</head>
+
+<body>
+    <div id="container">
+        <header class="header">
+            <h1>Flash Cards</h1>
+        </header>
+        <main>
+            <div class="col6">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, laborum alias aperiam quisquam sapiente esse quidem repudiandae consectetur commodi nihil, nulla pariatur accusamus eius at. Cupiditate natus sit dolorem ipsum veniam quibusdam labore, minima, quis necessitatibus soluta laudantium neque est veritatis, aliquam at architecto placeat modi! Dolorum amet optio numquam.</p>
+                <div>
+                    <button id="button1">Sign Up</button>
+                    <!--Sign Up Form-->
+                    <div class="signup">
+                        <form action="">
+
+                        </form>
+                    </div>
+                    <button id="button2">Login</button>
+                    <!--Login Form-->
+                    <div class="login">
+                        <form action="">
+
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div><img src="./img/flashC.jpg"></div>
+        </main>
+        <footer class=>
+
+            <div class="card">
+                <p>All Right Reserved &copy; 2020-2021</p>
+            </div>
+
+        </footer>
+    </div>
+    <script src="./js/main.js"></script>
+</body>
+
+</html>
