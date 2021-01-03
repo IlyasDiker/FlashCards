e61bb272b68ee9a0dda5da8831ea0ca6e58933b3
SirOphis <moses.belfatmi@gmail.com>
2021-01-03 13:05:49 +0100

    update the index and css

--- index.php
+++ index.php
@@ -20,33 +20,38 @@
             <div class="col6">
                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate, laborum alias aperiam quisquam sapiente esse quidem repudiandae consectetur commodi nihil, nulla pariatur accusamus eius at. Cupiditate natus sit dolorem ipsum veniam quibusdam labore, minima, quis necessitatibus soluta laudantium neque est veritatis, aliquam at architecto placeat modi! Dolorum amet optio numquam.</p>
                 <div>
-                    <button id="button1">Sign Up</button>
+                    <button id="signup" class="btn">Sign Up</button>
                     <!--Sign Up Form-->
                     <div class="signup">
                         <form action="">
 
                         </form>
                     </div>
-                    <button id="button2">Login</button>
+                    <button id="login" class="btn">Login</button>
                     <!--Login Form-->
-                    <div class="login">
-                        <form action="">
-
+                    <!--js aint working-->
+                    <div class="login card">
+                        <form action="#" method="post">
+                            <label for="username">Username :</label>
+                            <input type="text" name="username" id="username">
+                            <label for="pass">Password:</label>
+                            <input type="password" name="pass" id="pass">
+                            <input type="submit" value="login" class="btn">
                         </form>
                     </div>
                 </div>
             </div>
             <div><img src="./img/flashC.jpg"></div>
         </main>
-        <footer class=>
+        <footer>
 
-            <div class="card">
+            <div>
                 <p>All Right Reserved &copy; 2020-2021</p>
             </div>
 
         </footer>
     </div>
-    <script src="./js/main.js"></script>
+    <script src="/js/main.js"></script>
 </body>
 
 </html>
---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById("login").addEventListener("click",()=>{
+    document.getElementsByClassName("login")[0].style.display="block";
+    alert("test");
+})
